1e566d9213fc8dee4ad3cea414688c5c66c631b0
seketman-andreani <ddemarziani@andreani.com>
2025-09-25 14:59:04 -0300

    fix: fastapi deprecation fixes.

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def register_oidc_clients():
             client_kwargs={"scope": "openid profile email offline_access"},
         )
 
-@app.on_event("startup")
+@app.on_lifespan
 async def startup_event():
     register_oidc_clients()
 
